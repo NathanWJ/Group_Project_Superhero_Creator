aa2352d8fdab03f5602cc7fe4023335a4d924ee2
natejack <jetanjetan@hotmail.com>
2022-07-21 11:21:51 -0700

    First commit for group

---
+++ client/src/components/HeroLogin.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; 
+
+const HeroLogin = (props) => { 
+
+    return ( 
+        <div>
+            <h1>Login Hero Page</h1>
+        </div>
+    )
+}
+
+export default HeroLogin;
