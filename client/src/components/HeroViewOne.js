f33e5f2b4080aa13d663bba479655ecfd64e1ec3
Christian Ramirez <christian.r0602@gmail.com>
2022-07-27 10:06:01 -0500

    viewOne component

--- client/src/components/HeroViewOne.js
+++ client/src/components/HeroViewOne.js
@@ -1,12 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'; 
+import { useParams } from 'react-router-dom'
 
 const HeroViewOne = (props) => { 
+    const [name, setName] = useState('')
+    const [powers, setPowers] = useState('')
+    const [weakness, setWeakness] = useState('')
+    const {id} = useParams()
 
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/heroes/${id}`)
+        .then(res => {
+            setName(res.data.name)
+            setPowers(res.data.powers)
+            setWeakness(res.data.weakness)
+        })
+        .catch(err => console.log(err))
+    })
     return ( 
         <div>
-            <h1>View One Hero Page</h1>
+        <div>
+            <h2>Superhero Name:</h2>
+            <p>{name}</p>
         </div>
+        <div>
+            <h2>Superhero's Powers:</h2>
+            <p>{powers}</p>
+        </div>
+        <div>
+            <h2>Superhero's Weakness:</h2>
+            <p>{weakness}</p>
+        </div>
+        </div>
+ 
     )
 }
 
